bcfdf4d013423af8aa476933c44ed360af355504
DMCustoms <sve16514968@yandex.ru>
2023-05-29 19:03:42 +0300

    v 0.0.1

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,7 @@ gulp.task('server', function () {
     });
 
     gulp.watch("src/*.html").on('change', browserSync.reload)
+    gulp.watch("src/scss/*.scss").on('change', browserSync.reload)
     gulp.watch("src/js/*.js").on('change', browserSync.reload)
 });
 
@@ -36,7 +37,7 @@ gulp.task('styles', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch("src/scss/**/*.sass|css|scss", gulp.parallel('styles'));
+    gulp.watch("src/scss/*.scss").on("change", gulp.parallel('styles'));
     gulp.watch("src/*.html").on("change", gulp.parallel('html'));
     gulp.watch("src/js/*.*").on("change", gulp.parallel('scripts'));
 });
